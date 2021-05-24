56b3b472a13e4cd84b69b79251aa529f7d592d43
jinhyeok6941 <66781994+jinhyeok6941@users.noreply.github.com>
2021-05-25 08:56:21 +0900

    d

--- Assets/CJH/Scripts/ChangeZeroGravity.cs
+++ Assets/CJH/Scripts/ChangeZeroGravity.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ChangeZeroGravity : MonoBehaviour
 {
+    Rigidbody rigid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            float dist = Vector3.Distance(transform.position, other.transform.position);
+            transform.forward = other.transform.position - transform.position;
+            if (dist <= 30)
+            {
+                rigid.AddForce(other.transform.forward * 0.01f, ForceMode.Impulse);
+            }
+            else
+            {
+                Vector3 dir = other.transform.position - transform.position;
+                rigid.AddForce(dir * 0.01f, ForceMode.Impulse);
+            }
+
+        }
     }
 }
