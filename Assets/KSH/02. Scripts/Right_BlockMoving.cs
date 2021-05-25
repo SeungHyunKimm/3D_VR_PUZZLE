70407ba1eb8197d73373e73365aad3cba396f8e2
SeungHyunKimm <skyblue051@naver.com>
2021-05-25 18:57:51 +0900

    ..

--- Assets/KSH/02. Scripts/Right_BlockMoving.cs
+++ Assets/KSH/02. Scripts/Right_BlockMoving.cs
@@ -7,6 +7,7 @@ public class Right_BlockMoving : MonoBehaviour
     Transform catchObj;
     public float throwPower = 3;
     RaycastHit hit;
+    Rigidbody rigid;
 
     void Start()
     {
@@ -34,19 +35,27 @@ public class Right_BlockMoving : MonoBehaviour
             int layer = 1 << LayerMask.NameToLayer("Puzzle");
             if (Physics.SphereCast(ray, 1f, out hit, 100, layer))
             {
-                print("부딪혐");
+                print("부딪힘");
+                catchObj = hit.transform;
                 hit.transform.SetParent(transform);
+                //오른손과 큐브와의 거리를 구하자
+                Vector3 dir = transform.position - hit.transform.position;
+                //자연스럽게 손 안으로 올 수 있게끔 연출하자
+                //rigid.AddForce(dir * 0.01f);
+                //hit.transform.position += dir * 0.01f * Time.deltaTime;
+                //hit.transform.position = rigid.AddForce(dir * 0.01f);
                 hit.transform.position = transform.position + new Vector3(0,0,1);
-                //print(transform.position);
-                //print(Camera.main.transform.position);
+                
             }
         }
+
     }
 
     void DropObj()
     {
         if (catchObj == null) return;
-        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+
+        if (OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.RTouch))
         {
 
             catchObj.SetParent(null);
@@ -62,8 +71,10 @@ public class Right_BlockMoving : MonoBehaviour
 
     void ThrowObj()
     {
+
         Rigidbody rb = catchObj.GetComponent<Rigidbody>();
         rb.velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch) * throwPower;
+
     }
 
 }
