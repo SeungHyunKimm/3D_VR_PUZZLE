ba8c206b51c8890ea091ec377fe51cb75e2b8476
SeungHyunKimm <skyblue051@naver.com>
2021-05-25 18:16:51 +0900

    ..

--- Assets/KSH/02. Scripts/Right_BlockMoving.cs
+++ Assets/KSH/02. Scripts/Right_BlockMoving.cs
@@ -6,6 +6,7 @@ public class Right_BlockMoving : MonoBehaviour
 {
     Transform catchObj;
     public float throwPower = 3;
+    RaycastHit hit;
 
     void Start()
     {
@@ -23,18 +24,21 @@ public class Right_BlockMoving : MonoBehaviour
 
     void CatchObj()
     {
-        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
         {
             Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
+
 
             print("½÷Áü");
 
             int layer = 1 << LayerMask.NameToLayer("Puzzle");
-            if (Physics.Raycast(ray, out hit, 100, layer))
+            if (Physics.SphereCast(ray, 1f, out hit, 100, layer))
             {
                 print("ºÎµúÇø");
-                hit.transform.position = transform.position;
+                hit.transform.SetParent(transform);
+                hit.transform.position = transform.position + new Vector3(0,0,1);
+                //print(transform.position);
+                //print(Camera.main.transform.position);
             }
         }
     }
@@ -42,12 +46,17 @@ public class Right_BlockMoving : MonoBehaviour
     void DropObj()
     {
         if (catchObj == null) return;
-        if(OVRInput.GetUp(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
         {
+
             catchObj.SetParent(null);
-            catchObj.GetComponent<Rigidbody>().isKinematic = false;
+            
+            //catchObj.GetComponent<Rigidbody>().isKinematic = false;
+
             ThrowObj();
+
             catchObj = null;
+
         }
     }
 
