0c1d1badd6980ea04985db84a36522945a96888c
jinhyeok6941 <66781994+jinhyeok6941@users.noreply.github.com>
2021-05-31 10:56:25 +0900

    c

--- Assets/Main/Scripts/ButtonManager.cs
+++ Assets/Main/Scripts/ButtonManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -15,18 +16,18 @@ public class ButtonManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void OnClickExit()             //나가기
+    public void OnClickCancle()             //나가기
     {
         gameObject.SetActive(false);
     }
 
     public void OnClickRetry()            //재도전
     {
-
+        SceneManager.LoadScene(2);
     }
 
     public void OnClickOther()            //다른 목록보기
     {
-
+        SceneManager.LoadScene(1);
     }
 }
