8ff6e1fa47363851d23a2bf4a24296b1c81cd146
jinhyeok6941 <66781994+jinhyeok6941@users.noreply.github.com>
2021-05-31 11:01:08 +0900

    d

--- Assets/Main/Scripts/ButtonManager.cs
+++ Assets/Main/Scripts/ButtonManager.cs
@@ -5,7 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public enum ButtonState
+    {
+        Start,
+        Select,
+        Mode_A,
+        Mode_B,
+        Mode_C,
+        Mode_D
+    }
+
+    public ButtonState state;
+
     void Start()
     {
 
@@ -15,19 +26,47 @@ public class ButtonManager : MonoBehaviour
     {
     }
 
-    // Update is called once per frame
-    public void OnClickCancle()             //나가기
+    public void OnClickCancel()             //나가기
     {
         gameObject.SetActive(false);
     }
 
     public void OnClickRetry()            //재도전
     {
-        //SceneManager.LoadScene();
+
     }
 
     public void OnClickOther()            //다른 목록보기
     {
+
+    }
+
+    public void OnClickStart()      //클릭시 스타트
+    {
         SceneManager.LoadScene(1);
+        ;
+    }
+    public void OnClickMode_A()     // Mode_A Scene Load
+    {
+        SceneManager.LoadScene(2);
+
+
+    }
+    public void OnClickMode_B()     // Mode_B Scene Load
+    {
+        SceneManager.LoadScene(3);
+
+
+    }
+    public void OnClickMode_C()     // Mode_C Scene Load
+    {
+        SceneManager.LoadScene(4);
+
+
+    }
+    public void OnClickMode_D()     // Mode_D Scene Load
+    {
+        SceneManager.LoadScene(5);
+
     }
 }
